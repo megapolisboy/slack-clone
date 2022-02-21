1de1e353ac853c263198eaca4d6b37bf12271e87
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-02-21 17:39:21 +0200

    Header section of chat implemented

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,17 +1,59 @@
+import { InfoOutlined, StarBorderOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
 const Chat = () => {
   return (
     <ChatContainer>
-      <h1>I am the chat screen</h1>
+      <Header>
+        <HeaderLeft>
+          <h4>
+            <strong>#RoomName</strong>
+          </h4>
+          <StarBorderOutlined />
+        </HeaderLeft>
+        <HeaderRight>
+          <p>
+            <InfoOutlined /> Details
+          </p>
+        </HeaderRight>
+      </Header>
     </ChatContainer>
   );
 };
 
 export default Chat;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+    margin-right: 10px;
+  }
+
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 20px;
+    font-size: 18px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+`;
 
 const ChatContainer = styled.div`
   flex: 0.7;
